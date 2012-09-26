875538aa5fa7cee85e4a5379df033cd139fed67a
Hamish <chaokel.mail@gmail.com>
2012-09-26 22:12:32 +0800

    sfgsdg

---
+++ AbstractWorld.cpp
@@ -0,0 +1,21 @@
+#include "AbstractWorld.h"
+
+AbstractWorld* AbstractWorld::currentInstance = NULL;
+
+AbstractWorld::AbstractWorld(void)
+{
+}
+
+
+AbstractWorld::~AbstractWorld(void)
+{
+}
+
+
+void AbstractWorld::GameLoop(void)
+{
+	Init();
+	currentInstance = this;
+	glutDisplayFunc(AbstractWorld::displayCallback);
+	glutKeyboardFunc(AbstractWorld::keyboardCallback);
+}
---
+++ GameWorld.cpp
@@ -0,0 +1,67 @@
+#include "GameWorld.h"
+
+
+
+GameWorld::GameWorld(void)
+{
+}
+
+
+GameWorld::~GameWorld(void)
+{
+}
+
+
+void GameWorld::Display(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f ); 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glFlush();
+	glutSwapBuffers();
+}
+
+void GameWorld::Keyboard(unsigned char Key, int KeyX, int KeyY)
+{
+	toupper(Key);
+	switch(Key)
+	{
+		case 'W'://move camera up
+		{
+			GameWorld::Camera.MoveCamera(0, 0, -0.2);
+			break;
+		}
+		case 'A'://move camera to the left
+		{
+			GameWorld::Camera.MoveCamera(0.2, 0, 0);
+			break;
+		}
+		case 'S'://move camera down
+		{
+			GameWorld::Camera.MoveCamera(0, 0, 0.2);
+			break;
+		}
+		case 'D'://move camera right
+		{
+			GameWorld::Camera.MoveCamera(-0.2, 0, 0);
+			break;
+		}
+	}
+
+}
+
+void GameWorld::Mouse(int Button, int State, int MouseX, int MouseY)
+{
+
+}
+
+void GameWorld::SpecialKeyFunc(void)
+{
+
+}
+
+void GameWorld::Init(void)
+{
+
+}
