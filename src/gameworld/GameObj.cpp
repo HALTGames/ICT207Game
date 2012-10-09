201df6a16e49c39831dc4052b98022e6d603fdaf
Timothy Veletta <timothy.veletta@gmail.com>
2012-10-09 17:16:47 +0800

    Added some documentation to GameObj

--- src/gameworld/GameObj.cpp
+++ src/gameworld/GameObj.cpp
@@ -1,11 +1,13 @@
 #include "GameObj.h"
 
+//-----------------------------------------------------------------------------
 
 GameObj::GameObj(void)
 {
 	Init();
 }
 
+//-----------------------------------------------------------------------------
 
 GameObj::GameObj(const Vector3 pos, char* modelFile, 
 	const string collideType)
@@ -17,16 +19,22 @@ GameObj::GameObj(const Vector3 pos, char* modelFile,
 	collidableType = collideType;
 }
 
+//-----------------------------------------------------------------------------
+
 GameObj::~GameObj(void)
 {
 	model.~GameModel();
 }
+
+//-----------------------------------------------------------------------------
 
 void GameObj::Init()
 {
 	position = Vector3(0.0, 0.0, 0.0);
 	angle = 0.0f;
 }
+
+//-----------------------------------------------------------------------------
 
 void GameObj::Display()
 {
@@ -37,11 +45,15 @@ void GameObj::Display()
 	glPopMatrix();
 }
 
+//-----------------------------------------------------------------------------
+
 void GameObj::Update()
 {
 	// anything that changes on its own goes in here
 	// i.e. a scroll spinning around when its dropped
 }
+
+//-----------------------------------------------------------------------------
 
 bool GameObj::IsOnScreen() const
 {
@@ -49,47 +61,67 @@ bool GameObj::IsOnScreen() const
 
 }
 
+//-----------------------------------------------------------------------------
+
 bool GameObj::SetModel(char* modelFile)
 {
 	return model.LoadModel(modelFile);
 }
+
+//-----------------------------------------------------------------------------
 
 void GameObj::SetCollidableType(const string collideType)
 {
 	collidableType = collideType;
 }
 
+//-----------------------------------------------------------------------------
+
 void GameObj::SetPosition(const Vector3 pos)
 {
 	position = pos;
 }
+
+//-----------------------------------------------------------------------------
 
 void GameObj::ChangePosition(const Vector3 pos)
 {
 	position += pos;
 }
 
+//-----------------------------------------------------------------------------
+
 void GameObj::SetAngle(const int ang)
 {
 	angle = ang;
 }
+
+//-----------------------------------------------------------------------------
 
 void GameObj::ChangeAngle(const int ang)
 {
 	angle += ang;
 }
 
+//-----------------------------------------------------------------------------
+
 string GameObj::GetCollidableType() const
 {
 	return collidableType;
 }
+
+//-----------------------------------------------------------------------------
 
 Vector3 GameObj::GetPosition() const
 {
 	return position;
 }
 
+//-----------------------------------------------------------------------------
+
 int GameObj::GetAngle() const
 {
 	return angle;
 }
+
+//-----------------------------------------------------------------------------
--- src/gameworld/GameObj.h
+++ src/gameworld/GameObj.h
@@ -14,21 +14,76 @@ using namespace std;
 
 //-----------------------------------------------------------------------------
 
+/**
+ * \brief GameObj.h GameObj.cpp
+ * \class GameObj
+ * \brief GameObj class implementation.
+ *
+ * Classifies all objects in the game and gives them a position, angle, model
+ * and collidable type. 
+ *
+ * \author Timothy Veletta 
+ * \date 09/10/12
+ * \version 01 - Timothy Veletta, 09/10/12
+ *		Created constructors, get and set functions as well as initialising,
+ *		display and updating functions.
+ */
 class GameObj
 {
 public:
+	/**
+	 * \brief The default constructor.
+	 *
+	 * Calls the Init function.
+	 */
 	GameObj(void);
 
+	/**
+	 * \brief Secondary constructor.
+	 *
+	 * Sets the position, model file and collision type.
+	 *
+	 * \param pos the initial position of the object
+	 * \param modelFile the model associated with the object
+	 * \param collideType the collidable type of the object
+	 */
 	GameObj(const Vector3 pos, char* modelFile, const string collideType);
 
+	/**
+	 * \brief The default destructor.
+	 *
+	 * Destroys the game object.
+	 */
 	virtual ~GameObj(void);
 
+	/**
+	 * \brief Initialises the object.
+	 *
+	 * Sets the initial properties of the game object.
+	 */
 	virtual void Init();
 
+	/**
+	 * \brief Displays the object.
+	 *
+	 * Displays the object at the current position with the current angle.
+	 */
 	virtual void Display();
 
+	/**
+	 * \brief Updates the object.
+	 *
+	 * Updates the object every single loop. This would be used for anything
+	 * that changes on its own.
+	 */
 	virtual void Update();
 
+	/**
+	 * \brief 
+	 *
+	 *
+	 *
+	 */
 	bool IsOnScreen() const;
 
 
@@ -55,10 +110,10 @@ public:
 
 	// getBoundingBox?
 private:
-	GameModel model;
-	string collidableType;
-	Vector3 position;
-	int angle;
+	GameModel model; /**< the model of the game object */
+	string collidableType; /**< the collision type */
+	Vector3 position; /**< the current position of the object */
+	int angle; /**< the current angle of the object */
 };
 
 //-----------------------------------------------------------------------------
