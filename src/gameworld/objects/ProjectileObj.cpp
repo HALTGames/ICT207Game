3a6f4909b010fcf41a6329e0cc84b9dad3225061
Timothy Veletta <timothy.veletta@gmail.com>
2012-10-28 09:20:26 +0800

    Completed Projectile class, needs some testing

--- src/gameworld/objects/ProjectileObj.cpp
+++ src/gameworld/objects/ProjectileObj.cpp
@@ -1,11 +1,35 @@
 #include "ProjectileObj.h"
 
 
-ProjectileObj::ProjectileObj(void)
+ProjectileObj::ProjectileObj(Vector3 strt, Vector3 end)
 {
+	speed = 1.0;
+	length = 20.0;
+
+	start = strt;
+	position = strt;
+
+	direction = end - strt;
+	direction = direction.Normalise();
+
+	beginTime = glutGet(GLUT_ELAPSED_TIME);
 }
 
-
-ProjectileObj::~ProjectileObj(void)
+void ProjectileObj::Display()
 {
+	int timeDifference = (glutGet(GLUT_ELAPSED_TIME) - beginTime) / 1000;
+	double speedTime = speed * timeDifference;
+
+	position = direction * speedTime;
+
+	double distanceTravelled = (position - start).Length();
+
+	if(distanceTravelled < length)
+	{
+		GameObj::Display();
+	}
+	else
+	{
+		//destroy this object
+	}
 }
--- src/gameworld/objects/ProjectileObj.h
+++ src/gameworld/objects/ProjectileObj.h
@@ -1,12 +1,24 @@
 #pragma once
 
 #include "..\gameobj.h"
+#include "../../Vector3.h"
 
 class ProjectileObj :
 	public GameObj
 {
 public:
-	ProjectileObj(void);
-	~ProjectileObj(void);
+	ProjectileObj(Vector3 start, Vector3 end);
+
+	virtual void Display();
+
+protected:
+	double speed;
+	double length;
+
+private:
+	Vector3 start;
+	Vector3 direction;
+
+	int beginTime;
 };
 
