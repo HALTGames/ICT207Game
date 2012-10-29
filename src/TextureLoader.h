1bac6c5da0f0d2c2b9c52f2452d1ada057d6f165
Hamish <chaokel.mail@gmail.com>
2012-10-29 19:26:34 +0800

    Implemented a simple texture loader
    needs testing.

--- src/TextureLoader.h
+++ src/TextureLoader.h
@@ -1,10 +1,24 @@
+#ifndef TEXTURELOADER_H
+#define TEXTURELOADER_H
 #pragma once
 
-//#include "SOIL.h"
+#include "SOIL.h"
+#include <string>
+#include <gl\glut.h>
+#include <vector>
+
 class TextureLoader
 {
 public:
 	TextureLoader(void);
 	~TextureLoader(void);
+
+	void LoadTexture(int i, std::string Name);
+
+	Gluint GetTexture(int i) const {return Texture[i];}
+
+private:
+	GLuint Texture[100];
 };
 
+#endif
