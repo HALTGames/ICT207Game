b706e367d624245908427813a06a2662129d6e0c
Timothy Veletta <timothy.veletta@gmail.com>
2012-11-03 18:03:16 +0800

    Started model importer stuff

--- src/modelimporter/ModelManager.h
+++ src/modelimporter/ModelManager.h
@@ -1,2 +1,16 @@
 #pragma once
 
+#include "Model.h"
+
+
+enum ModelEnum
+{
+
+};
+
+class ModelManager
+{
+	ModelManager();
+
+	//DrawModel(ModelEnum type, xPos, yPos, zPos,
+};
--- src/modelimporter/glm.h
+++ src/modelimporter/glm.h
@@ -1,15 +1,15 @@
 /*    
-      glm.h
-      Nate Robins, 1997, 2000
-      nate@pobox.com, http://www.pobox.com/~nate
+	  glm.h
+	  Nate Robins, 1997, 2000
+	  nate@pobox.com, http://www.pobox.com/~nate
  
-      Wavefront OBJ model file format reader/writer/manipulator.
+	  Wavefront OBJ model file format reader/writer/manipulator.
 
-      Includes routines for generating smooth normals with
-      preservation of edges, welding redundant vertices & texture
-      coordinate generation (spheremap and planar projections) + more.
+	  Includes routines for generating smooth normals with
+	  preservation of edges, welding redundant vertices & texture
+	  coordinate generation (spheremap and planar projections) + more.
 
-      Improved version:
+	  Improved version:
 	  Tudor Carean - April 2008 - added texture support
 
  */
